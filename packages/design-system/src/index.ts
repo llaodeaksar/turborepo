65bbdb8d69fc12b517baa11eb8775b5c431192fa
llaodeaksar <laodeaksarr@gmail.com>
2022-05-30 15:07:51 +0000

    add jest and esbuild file

---
+++ packages/design-system/src/index.ts
@@ -0,0 +1,12 @@
+/** LIB **/
+export {
+   // Shadows,
+    styled,
+    css,
+    keyframes,
+    getCssText,
+    theme,
+    config,
+  } from './lib/stitches.config';
+//  export { globalStyles } from './lib/globalStyles';
+  export type { CSS, VariantProps } from './lib/stitches.config';
